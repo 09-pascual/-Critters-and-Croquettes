2f642f029ec7967fe45edb4cc4737ecce4d0b5f1
pascual ramirez Valeriano <pascualramirez27@gmail.com>
2024-08-16 10:06:49 -0400

    walking classes can feed and have shifts now

--- index.py
+++ index.py
@@ -1,15 +1,13 @@
-# index.py
-
 from walking import Llama, Donkey, Goat, Sheep, Pig
 from slithering import Copperhead, RatSnake, BoaConstrictor, Python, GarterSnake
 from swimming import Goldfish, Mallard, KoiFish, Frog, Turtle
 
 # Creating instances of each critter with name and species
-miss_fuzz = Llama("Miss Fuzz", "Llama")
-lil_bro = Donkey("Mr. Little", "Donkey")
-ronaldo = Goat("Ronaldo", "Goat")
-wooly = Sheep("Wooly", "Sheep")
-porky = Pig("Porky", "Pot-bellied Pig")
+miss_fuzz = Llama("Miss Fuzz", "domestic llama", "morning", "Llama Chow" )
+lil_bro = Donkey("Mr. Little", "donkey", "morning", "Donkey Chow")
+ronaldo = Goat("Ronaldo", "goat", "afternoon", "Goat chow")
+wooly = Sheep("Wooly", "sheep", "midday")
+porky = Pig("Porky", "pot-bellied pig", "morning")
 
 copper = Copperhead("Copper", "Copperhead Snake")
 rattles = RatSnake("Rattles", "Rat Snake")
@@ -23,9 +21,20 @@ koiboy = KoiFish("KoiBoy", "Koi Fish")
 hoppy = Frog("Hoppy", "Frog")
 slowpoke = Turtle("Slowpoke", "Turtle")
 
+#feeding walking animals
+miss_fuzz.feed()
+lil_bro.feed()
+
+
+# Testing
+print(f'{lil_bro.name} the {lil_bro.species} is available to pet during the {lil_bro.shift} shift.')
+print(f'{ronaldo.name} the {ronaldo.species} is available to pet during the {ronaldo.shift} shift.')
+print(f'{wooly.name} the {wooly.species} is available to pet during the {wooly.shift} shift.')
+print(f'{porky.name} the {porky.species} is available to pet during the {porky.shift} shift.')
+
 # Printing each critter's information
-print(miss_fuzz)
 print(lil_bro)
+print(miss_fuzz )
 print(ronaldo)
 print(wooly)
 print(porky)
